f7b2bd7fb58bb6cf2abb9074d13c51809279224e
calliekl <calliekl@naver.com>
2018-11-21 13:31:24 +0900

    shoot bullet
    총알 쏘는 코드 추가

--- Bear Farm/Assets/Scripts/MovePlayer.cs
+++ Bear Farm/Assets/Scripts/MovePlayer.cs
@@ -10,6 +10,10 @@ public class MovePlayer : MonoBehaviour
     private Vector3 pos;
     private Vector3 pos2;
 
+    public int count = 0;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
     private Rigidbody rb;
     private Transform vrCamera;
 
@@ -24,20 +28,41 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Vector3 movement= speed * new Vector3(vrCamera.TransformDirection(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).x, 0f, vrCamera.TransformDirection(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).z);
-        //movement.y = rb.velocity.y;
-        //rb.velocity = movement;
 
-        //pos = transform.position;
-            movePlayer();
+        movePlayer();
+
+        //마우스 클릭하면 총알이 발사된다
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
 
     }
 
+    //player가 보고 있는 시선 방향에 따라 움직인다
     public void movePlayer()
     {
         Vector3 movement = speed * new Vector3(vrCamera.TransformDirection(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).x, 0f, vrCamera.TransformDirection(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).z);
         //Vector3 movement = speed * new Vector3(-vrCamera.InverseTransformDirection(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).z, 0f, -vrCamera.InverseTransformDirection(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).x);
         movement.y = rb.velocity.y;
         rb.velocity = movement;
+    }
+
+    public void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        bullet.transform.position = transform.position + Camera.main.transform.forward * 2;
+        bullet.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * 10;
+        // Add velocity to the bullet
+        //bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 4;
+        //bullet.GetComponent<Rigidbody>().AddForce(transform.up * 10);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
     }
 }
