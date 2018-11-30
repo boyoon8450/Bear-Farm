2125747e46a66f72b07ee10bcfa0fc7bbe0cea14
Callie <calliekl@naver.com>
2018-11-30 12:35:39 +0900

    181130 Timer

---
+++ Bear Farm/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    int startTime=30;
+    float currentTime;
+
+	// Use this for initialization
+	void Start () {
+        //startTime = Time.time;
+        currentTime = startTime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //float t = Time.time - startTime;
+        currentTime -= 1 * Time.deltaTime;
+        //print(currentTime);
+        timerText.text = ((int)currentTime).ToString();
+
+        if(currentTime <=0)
+        {
+            currentTime = 0;
+        }
+
+	}
+}
