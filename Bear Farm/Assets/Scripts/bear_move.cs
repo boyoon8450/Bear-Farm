c772dd331f89f801f615b1537046d4d3bb060014
NinaNyo <40755967+NinaNyo@users.noreply.github.com>
2018-12-02 22:24:35 +0900

    181202
    -새로운 plane으로 배경 제작
    -곰돌이 움직임

---
+++ Bear Farm/Assets/Scripts/bear_move.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class bear_move : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    NavMeshPath path;
+    public float timeforNewPath;
+    bool inCoRoutine;
+
+
+    Vector3 target;
+    bool validPath;
+
+
+    //to set animation
+    public GameObject animate;
+    AnimatorController animating;
+    bool wait;
+
+    void Awake()
+    {
+        animating = animate.GetComponent<AnimatorController>();
+        wait = true;
+
+    }
+
+        // Use this for initialization
+        void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!inCoRoutine) {
+            
+            StartCoroutine(DoSomethig());
+
+        }
+
+        //if(wait)
+        //    animating.SetInt("animation, 15");
+        //else
+        //    animating.SetInt("animation, 1");
+
+    }
+
+    Vector3 getNewRandomPosition()
+    {
+        float x = Random.Range(-10, 10);
+        float z = Random.Range(-10, 10);
+
+        Vector3 pos = new Vector3(x, 0, z);
+        return pos;
+    }
+
+    IEnumerator DoSomethig()
+    {
+
+        inCoRoutine = true;
+        Debug.Log("wait");
+        wait = true;
+        yield return new WaitForSeconds(timeforNewPath);
+        Debug.Log("done wait");
+        wait = false;
+
+
+        GetNewPath();
+        validPath = navMeshAgent.CalculatePath(target, path);
+        if (!validPath)
+            Debug.Log("Found an invalid path");
+
+        while (!validPath)
+        {
+            //animating.SetInt("animation, 1");
+            yield return new WaitForSeconds(0.01f);
+           // animating.SetInt("animation, 15");
+            GetNewPath();
+            validPath = navMeshAgent.CalculatePath(target, path);
+        }
+        inCoRoutine = false;
+
+    }
+
+    void GetNewPath()
+    {
+        //animating.SetInt("animation, 15");
+        target = getNewRandomPosition();
+        navMeshAgent.SetDestination(target);
+        //animating.SetInt("animation, 1");
+
+    }
+
+
+
+
+}
