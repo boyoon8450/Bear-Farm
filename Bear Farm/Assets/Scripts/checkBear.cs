8825f6665ed224ff535b2a85b205de3c5e023228
mozzi0131 <sue03094@gmail.com>
2018-12-14 14:44:59 +0900

    gallery+data
    곰생성관련 코드 다소 수정 및 갤러리 완료

--- Bear Farm/Assets/Scripts/checkBear.cs
+++ Bear Farm/Assets/Scripts/checkBear.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class checkBear : MonoBehaviour {
-    
+public class checkBear : MonoBehaviour
+{
+
     //낮이면 0, 밤이면 1을 매개변수로 보냄
     //그리고 친밀도/점수를 두번째 매개변수로 전송한다
-	public void canGetBear(int day, int num)
+    public static void canGetBear(int day, int num)
     {
+        Debug.Log("cangetbear : num is " + num);
         int temp = returnIndex(day, num);
-        if(temp!= -1)
+        Debug.Log("temp is " + temp);
+        if (temp != -100)
         {
             switch (day)
             {
@@ -19,6 +22,7 @@ public class checkBear : MonoBehaviour {
                         //이 경우, 현재 가지고 있는 곰의 개수보다 가질 수 있는 곰의 개수가 많은 것임
                         //따라서 데이터 업데이트
                         DataManager.updateBearInfo(day, temp);
+                        Debug.Log("intimacy : bear created!!");
                     }
                     break;
                 case 1:
@@ -27,35 +31,42 @@ public class checkBear : MonoBehaviour {
                         //이 경우, 현재 가지고 있는 곰의 개수보다 가질 수 있는 곰의 개수가 많은 것임
                         //따라서 데이터 업데이트
                         DataManager.updateBearInfo(day, temp);
+                        Debug.Log("score : bear created!!");
                     }
                     break;
             }
         }
         
+
+
     }
 
-    int returnIndex(int day, int num)
+    static int returnIndex(int day, int num)
     {
-        int index = -1;
+        Debug.Log("returnIndex : day is " + day + " and num is " + num);
+        int index = -100;
         int i = 0;
 
         switch (day)
         {
             case 0:
-                for (i = 0; i < DataManager.STANDARDSIZE; i++)
+                for (i = 1; i < DataManager.STANDARDSIZE; i++)
                 {
-                    if (num < DataManager.intimacyStandard[i])
+                    if (num > DataManager.intimacyStandard[0] && num <= DataManager.intimacyStandard[i])
                     {
-                        return i - 1; // 해금될 수 있는 index의 최대를 return해줌
+                        Debug.Log("return 0 !!!"+num);
+                        return i-1; // 해금될 수 있는 index의 최대를 return해줌
                     }
                 }
                 break;
             case 1:
                 for (i = 0; i < DataManager.STANDARDSIZE; i++)
                 {
-                    if (num < DataManager.scoreStandard[i])
+                    //Debug.Log("scoreStandard[" + i + "] is " + DataManager.scoreStandard[i]);
+                    if (num >= DataManager.scoreStandard[0] && num <= DataManager.scoreStandard[i])
                     {
-                        return i - 1; // 해금될 수 있는 index의 최대를 return해줌
+                        Debug.Log("return1!!!");
+                        return i-1; // 해금될 수 있는 index의 최대를 return해줌
                     }
                 }
                 break;
