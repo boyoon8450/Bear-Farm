d42967a2fbb8bcce1d6adfe8b50b848fd8d822cf
calliekl <calliekl@naver.com>
2018-12-14 18:34:31 +0900

    181214 수현과 예진 합침
    skybox도 바꿈

--- Bear Farm/Assets/Scripts/pauseMenu.cs
+++ Bear Farm/Assets/Scripts/pauseMenu.cs
@@ -6,13 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class pauseMenu : MonoBehaviour
 {
-    public bool pause;
+    static public bool pause;
+    static public bool galleryOn;
     public bool optionOn;
     bool GalleryOn;
     public GameObject pauseMenuPanel;
     //public GameObject titleMenu;
     public GameObject optionMenu;
     public GameObject camera;
+
+    public GameObject galleryM;
     AudioSource source;
 
     // Use this for initialization
@@ -23,6 +26,7 @@ public class pauseMenu : MonoBehaviour
         Time.timeScale = 1;//scene 로드될때 사물들이 움직이게 한다
         optionMenu.SetActive(false);
         pauseMenuPanel.SetActive(false);
+        galleryM = GameObject.Find("GalleryManager");
         source = gameObject.GetComponent<AudioSource>();
 
     }
@@ -30,7 +34,7 @@ public class pauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && !optionOn)//뒤로를 누르면 정지메뉴 나타난다 이때 option 창도 커져있으면 안된다
+        if (Input.GetKeyDown(KeyCode.Escape) && !optionOn && !galleryOn)//뒤로를 누르면 정지메뉴 나타난다 이때 option 창도 커져있으면 안된다
         {
             onPause();
         }
@@ -88,6 +92,14 @@ public class pauseMenu : MonoBehaviour
         pauseMenuPanel.SetActive(false);
         optionMenu.SetActive(true);
         optionOn = true;
+    }
+
+    public void onGalleryClick()
+    {
+        galleryOn = true;
+        galleryM.GetComponent<GalleryManager>().showGallery();
+        pauseMenuPanel.SetActive(false);
+        //optionMenu.SetActive(false);
     }
 
 }
