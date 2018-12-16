1783b3fcf40178e6eda27ba652e978a8e42519af
NinaNyo <kumote@naver.com>
2018-12-16 14:48:39 +0900

    181216 신인아
    -옵션에서 소리 끄고 키고 완성

---
+++ Bear Farm/Assets/musicManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class musicManager : MonoBehaviour {
+
+    GameObject DayNightManager;
+    AudioSource bgm;
+    Toggle t;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        DayNightManager = GameObject.Find("DayNightManager");
+        bgm = DayNightManager.GetComponent<AudioSource>();
+        t = gameObject.GetComponent<Toggle>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void valueChanged()
+    {
+        if (t.isOn)
+        {
+            bgm.mute = false;
+        }
+        else
+        {
+            bgm.mute = true;
+        }
+    }
+}
---
+++ Bear Farm/Assets/soundeffectManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class soundeffectManager : MonoBehaviour {
+
+
+    AudioSource Player_audio;
+    AudioSource Enemy_audio;
+    AudioSource Menu_audio;
+    AudioSource Managers_audio;
+    AudioSource Enemy_audio2;
+    Toggle t;
+
+
+    // Use this for initialization
+    void Start () {
+
+        Player_audio = GameObject.Find("Player").GetComponent<AudioSource>();
+        Enemy_audio = GameObject.Find("Enemy").GetComponent<AudioSource>();
+        Menu_audio = GameObject.Find("MenuController").GetComponent<AudioSource>();
+        Managers_audio = GameObject.Find("Managers").GetComponent<AudioSource>();
+        Enemy_audio2 = GameObject.Find("EnemyOriginal").GetComponent<AudioSource>();
+
+
+
+        t = gameObject.GetComponent<Toggle>();
+    }
+
+
+    public void valueChanged()
+    {
+        if (t.isOn)
+        {
+            Player_audio.mute = false;
+            Enemy_audio.mute = false;
+            Menu_audio.mute = false;
+            Managers_audio.mute = false;
+            Enemy_audio2.mute = false;
+        }
+        else
+        {
+            Player_audio.mute = true;
+            Enemy_audio.mute = true;
+            Menu_audio.mute = true;
+            Managers_audio.mute = true;
+            Enemy_audio2.mute = true;
+            
+        }
+    }
+}
